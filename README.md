16e8cefa3f29967b60b93af7781f396531cf0772
ayyysh04z <ayush.yadav@zoop.one>
2024-05-18 02:37:32 +0530

    readme additon

--- README.md
+++ README.md
@@ -5,6 +5,23 @@ application up and running.
 
 Things you may want to cover:
 
+* GUIDE : https://medium.com/swlh/how-to-build-an-api-with-ruby-on-rails-28e27d47455a
+
+* API Endpoints
+- GET : `curl -X GET http://localhost:3000/api/v1/products`
+- POST : `curl -X POST http://localhost:3000/api/v1/products \
+  -H "Content-Type: application/json" \
+  -d '{
+        "product": {
+          "name": "CJ890 Series Desktop Curved Monitor",
+          "brand": "Samsung",
+          "price": "$1099.99",
+          "description": "49 inch 3840x1080 Super Ultra-Wide Desktop Monitor for Business, 144 Hz, USB-C, HDMI, DisplayPort, 3-Year Warranty (C493890DKN)"
+        }
+      }'`
+- GET : `curl -X GET http://localhost:3000/api/v1/products/1`
+
+
 * Commands use to create
 - `rails g model product name:string brand:string price:string description:string --no-helper --no-assets --no-template-engine --no-test-framework`
 - `rails g controller api/v1/products index show --no-helper --no-assets --no-template-engine --no-test-framework`
--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::ProductsController < ApplicationController
 
-  # index renders all items in the products table
+  # Index Action :index renders all items in the products table
   def index
     products = Product.all
     render json: products, status: 200
   end
 
-  # As the name implies this action lets us create a new product.
+  # Create Action :As the name implies this action lets us create a new product.
   # If the product saves correctly, we render the json data for the product.
   # If the product does not save correctly we render an error object.
   def create
@@ -24,7 +24,7 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
-  # This method looks up the product by the id, if it is found we render the json object
+  # Show Action: This method looks up the product by the id, if it is found we render the json object
   # Otherwise we render an error object.
   def show
     product = Product.find_by(id: params[:id])
@@ -36,11 +36,15 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
-  # This private method is only available to this controller.
+  # Private Method for Action Controller Parameters :This private method is only available to this controller.
+  # https://api.rubyonrails.org/classes/ActionController/Parameters.html#method-i-require
   # It uses the the built-in methods .require and .permit provided by ActionController
+  # It's a common Rails convention to use strong parameters to sanitize and whitelist parameters coming from the client to prevent mass assignment vulnerabilities.
+
   private
   
   def prod_params
+    # specifies that the request must include a product parameter and permits only the specified attributes (:name, :brand, :price, :description).
     params.require(:product).permit([
       :name,
       :brand,
